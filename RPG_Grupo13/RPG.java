54f15ee558fe73789f7a300bdd30a5488beeb00e
Daniel Leite <daniel199257@gmail.com>
2021-11-08 23:56:49 -0300

    Add methods

--- RPG_Grupo13/RPG.java
+++ RPG_Grupo13/RPG.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
+
+import java.util.Random;
 import java.util.Collections;
 import java.util.ArrayList;
+
 public class RPG {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner scr = new Scanner(System.in);
+		int contarPontos = 0;
+		String nomePersonagem = "";
 		
 	int opcao;
 	
@@ -26,7 +31,19 @@ public class RPG {
 			break;
 		case 2:
 			System.out.println("Jogar");
-			System.out.println("");
+			//nomePersonagem = nomePersonagem(scr);
+			
+			ArrayList opcoes = new ArrayList<String>();
+			
+			opcoes.add("programação");
+			opcoes.add("Structured Query Language (SQL)");
+			opcoes.add("raciocínio lógico");
+			opcoes.add("marcação de texto (Estruturação)");
+			opcoes.add("Nenhuma das anteriores");
+			
+			perguntasAleatorias(opcoes);
+			
+			System.out.println("teste");
 			break;
 		case 3:
 			System.out.println("« Créditos »");
@@ -39,5 +56,34 @@ public class RPG {
 		}
 	}
 	while(opcao == 4);
- }
+	
+	}
+	
+	
+	
+	public static void nomePersonagem(Scanner scr) {
+		System.out.println("Digite o nome do personagem: ");
+		String nomePersonagem = scr.next();
+		
+		return nomePersonagem;
+	}
+	
+	public static void nomeMago(Scanner scr) {
+		System.out.println("Digite o nome do personagem: ");
+		String nomeMago = scr.next();
+		return nomeMago;
+	}
+	
+	public static void perguntasAleatorias(ArrayList opcoes) {
+	    Collections.shuffle(opcoes);
+	    for (int indexOpcoes = 0; indexOpcoes < opcoes.size(); indexOpcoes++) {
+            System.out.println((indexOpcoes) + ") - " + opcoes.get(indexOpcoes));
+        }
+	}
+	
+	public static void ensinarJogo(String nomeJogador) {
+		System.out.println("Então, " + nomeJogador + ", para entender portas lógicas, primeiro vamos resolver um desafio" + "/n");
+	}
+	
+	
 }
